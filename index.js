d209cca4f370659d843fbb11ea7ad924296f11f7
vincent cupid tabiongan <cupidvincent@gmail.com>
2023-07-17 19:23:04 +0800

    init

---
+++ index.js
@@ -0,0 +1,32 @@
+import express from 'express'
+import dotenv from 'dotenv'
+
+dotenv.config();
+const port = process.env.PORT || 5000;
+import cookieParser from 'cookie-parser';
+// import userRoutes from './routes/userRoutes.js'
+import gameRoutes from './routes/gameRoute.js'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+import connectDB from './config/db.js';
+
+connectDB();
+
+const app = express();
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.use(cookieParser());
+
+app.use('/api/game',gameRoutes);
+app.get('/', (req, res) => res.send('Server is Ready'))
+
+app.use(notFound);
+app.use(errorHandler);
+
+app.listen(
+    port,
+    () => console.log(`Server started on PORT: ${port}`)
+)
+
+export default app;
