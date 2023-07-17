3dc6fb2af61b8abc130be827f0c5c0c4058d30de
vincent cupid tabiongan <cupidvincent@gmail.com>
2023-07-17 20:34:49 +0800

    remove vercel configs

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ import dotenv from 'dotenv'
 dotenv.config();
 const port = process.env.PORT || 5000;
 import cookieParser from 'cookie-parser';
-// import userRoutes from './routes/userRoutes.js'
 import gameRoutes from './routes/gameRoute.js'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import connectDB from './config/db.js';
@@ -28,5 +27,3 @@ app.listen(
     port,
     () => console.log(`Server started on PORT: ${port}`)
 )
-
-export default app;
