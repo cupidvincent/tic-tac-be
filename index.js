1d007d1aafd17542bf83892c047dc2b018c3cd12
vincent cupid tabiongan <cupidvincent@gmail.com>
2023-07-18 09:10:37 +0800

    added logs

--- index.js
+++ index.js
@@ -10,12 +10,19 @@ import connectDB from './config/db.js';
 
 connectDB();
 
+const logRequests = (req, res, next) => {
+    const now = new Date().toISOString();
+    console.log(`[${now}] ${req.method} ${req.url}`);
+    next(); // Call next() to proceed to the next middleware or route handler
+};
+
 const app = express();
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.use(cookieParser());
+app.use(logRequests);
 
 app.use('/api/game',gameRoutes);
 app.get('/', (req, res) => res.send('Server is Ready'))
